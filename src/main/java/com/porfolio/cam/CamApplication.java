c3d3c4252a2019ed0de427ef2e9c65a323790c41
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-27 17:37:21 -0300

    +Commit inicial añado application.properties por primera vez

---
+++ src/main/java/com/porfolio/cam/CamApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.cam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolio/cam/CamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.cam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
