bf00f5c4668ab55edb72e91385b936e58e44f40a
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-28 16:44:47 -0300

    add Mensaje

---
+++ src/main/java/com/porfolio/cam/Security/Controller/Mensaje.java
@@ -0,0 +1,23 @@
+package com.porfolio.cam.Security.Controller;
+public class Mensaje {
+    private String mensaje;
+    
+    //Constructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
