48e70b125b5512364102d40478f9f7ae82f4989d
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-28 13:14:26 -0300

    add RolService

---
+++ src/main/java/com/porfolio/cam/Security/Service/RolService.java
@@ -0,0 +1,25 @@
+
+package com.porfolio.cam.Security.Service;
+
+import com.porfolio.cam.Security.Entity.Rol;
+import com.porfolio.cam.Security.Enums.RolNombre;
+import com.porfolio.cam.Security.Repository.IRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    IRolRepository iRolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return iRolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        iRolRepository.save(rol);
+    }
+}
