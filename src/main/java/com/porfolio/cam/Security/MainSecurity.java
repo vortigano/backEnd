3d97f39ef047a7937e8100e0574a59ebb1793f03
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-28 17:23:39 -0300

    add MainSecurity con WebSecurityConfigurerAdapter

---
+++ src/main/java/com/porfolio/cam/Security/MainSecurity.java
@@ -0,0 +1,70 @@
+/*
+Control general del JWT
+*/
+package com.porfolio.cam.Security;
+
+import com.porfolio.cam.Security.Service.UserDetailsImpl;
+import com.porfolio.cam.Security.jwt.JwtEntryPoint;
+import com.porfolio.cam.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//Habilita la opción que los métodos que queremos proteger estarán bloqueados por esta opción
+//Es decir, tendremos que estar logeados previamente.
+public class MainSecurity extends WebSecurityConfigurerAdapter{
+    @Autowired
+    UserDetailsImpl userDetailsServicesImpl;
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+    
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/Auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServicesImpl).passwordEncoder(passwordEncoder());
+    }
+}
