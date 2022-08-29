855cd186ffe4fb4a84be23b01fc77bcba811c97c
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 12:55:09 -0300

    add Controller CExperiencia terminado

---
+++ src/main/java/com/porfolio/cam/Controller/CExperiencia.java
@@ -0,0 +1,86 @@
+package com.porfolio.cam.Controller;
+
+import com.porfolio.cam.Dto.DtoExperiencia;
+import com.porfolio.cam.Entity.Experiencia;
+import com.porfolio.cam.Security.Controller.Mensaje;
+import com.porfolio.cam.Service.SExperiencia;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("explab")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CExperiencia {
+    @Autowired
+    SExperiencia sExperiencia;
+    
+    //trae la lista
+    @GetMapping("/lista")
+    public ResponseEntity<List<Experiencia>> list(){
+        List<Experiencia> list= sExperiencia.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    //crea elementos
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody DtoExperiencia dtoExp){
+        if(StringUtils.isBlank(dtoExp.getNombreE()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if(sExperiencia.existsByNombreE(dtoExp.getNombreE()))
+            return new ResponseEntity(new Mensaje("Esa experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        //si pasa todas estas validaciones...
+        Experiencia experiencia = new Experiencia(dtoExp.getNombreE(), dtoExp.getDescripcionE());
+        sExperiencia.save(experiencia);
+        
+        return new ResponseEntity(new Mensaje("Experiencia agregada"), HttpStatus.OK);
+    }
+    
+    //actualiza la lista
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoExperiencia dtoExp){
+        //validación antes de actualizar
+        //valida el ID
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
+        
+        //compara nombres de las experiencias
+        if(!sExperiencia.existsByNombreE(dtoExp.getNombreE()) && 
+                sExperiencia.getByNombreE(dtoExp.getNombreE()).get().getId() != id){
+            return new ResponseEntity(new Mensaje("Esa experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        //no puede estar en blanco
+        if(StringUtils.isBlank(dtoExp.getNombreE()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        //finalmente actualiza el dato
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        experiencia.setNombreE(dtoExp.getNombreE());
+        experiencia.setDescripcionE(dtoExp.getDescripcionE());
+        
+        sExperiencia.save(experiencia);
+        return new ResponseEntity(new Mensaje("Experiencia actualizada"), HttpStatus.OK);
+    }
+    
+    //borrar dato
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        //valida el ID
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
+        
+        sExperiencia.delete(id);
+        
+        return new ResponseEntity(new Mensaje("Experiencia eliminada"), HttpStatus.OK);
+    }
+}
