bfca6772debcad21b6dbfe962215473742d1b710
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-30 20:57:49 -0300

    update dto DtoEducacion

--- src/main/java/com/porfolio/cam/Dto/DtoEducacion.java
+++ src/main/java/com/porfolio/cam/Dto/DtoEducacion.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.porfolio.cam.Dto;
 
-/**
- *
- * @author neo64
- */
+import javax.validation.constraints.NotBlank;
+
 public class DtoEducacion {
-    
+    //traemos los mismos campos excepto el ID
+    //no pueden estar vacíos
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+
+    public DtoEducacion() {
+    }
+
+    public DtoEducacion(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
 }
