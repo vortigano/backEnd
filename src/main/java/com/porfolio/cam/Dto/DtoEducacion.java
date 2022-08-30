da5276681fc182cacd7437a1b5be14d88f5d8348
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-30 20:55:08 -0300

    add dto DtoEducacion

---
+++ src/main/java/com/porfolio/cam/Dto/DtoEducacion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.porfolio.cam.Dto;
+
+/**
+ *
+ * @author neo64
+ */
+public class DtoEducacion {
+    
+}
