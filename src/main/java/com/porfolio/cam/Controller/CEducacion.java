66673a1a934f61a6dd27efcea67b5443f839eb9d
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-09-01 21:10:56 -0300

    update Controller CEducacion

--- src/main/java/com/porfolio/cam/Controller/CEducacion.java
+++ src/main/java/com/porfolio/cam/Controller/CEducacion.java
@@ -1,5 +1,89 @@
 package com.porfolio.cam.Controller;
 
+import com.porfolio.cam.Dto.DtoEducacion;
+import com.porfolio.cam.Entity.Educacion;
+import com.porfolio.cam.Security.Controller.Mensaje;
+import com.porfolio.cam.Service.Seducacion;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/educacion")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
+    @Autowired
+    Seducacion sEducacion;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Educacion>> list(){
+        List <Educacion> list= sEducacion.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Educacion> getById(@PathVariable("id") int id){
+        //verifica si existe o no el id
+        if(!sEducacion.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+        }
+        Educacion educacion = sEducacion.getOne(id).get();
+        return new ResponseEntity(educacion, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        //validaciones
+        if(!sEducacion.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+        }
+        sEducacion.delete(id);
+        return new ResponseEntity(new Mensaje("Educacion eliminada"), HttpStatus.OK); 
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody DtoEducacion dtoEducacion){
+        if(StringUtils.isBlank(dtoEducacion.getNombreE())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(sEducacion.existsByNombreE(dtoEducacion.getNombreE())){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Educacion educacion = new Educacion(dtoEducacion.getNombreE(), dtoEducacion.getDescripcionE());
+        sEducacion.save(educacion);
+        return new ResponseEntity(new Mensaje("Educacion creada"), HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoEducacion dtoEducacion){
+        if(!sEducacion.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el id"), HttpStatus.NOT_FOUND);
+        }
+        if(sEducacion.existsByNombreE(dtoEducacion.getNombreE()) 
+                && sEducacion.getByNombreE(dtoEducacion.getNombreE()).get().getId() != id){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        if(StringUtils.isBlank(dtoEducacion.getNombreE())){
+            return new ResponseEntity(new Mensaje("El campo no puede estar vacío"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Educacion educacion = sEducacion.getOne(id).get();
+        educacion.setNombreE(dtoEducacion.getNombreE());
+        educacion.setDescripcionE(dtoEducacion.getDescripcionE());
+        sEducacion.save(educacion);
+        return new ResponseEntity(new Mensaje("Educacion actualizada"), HttpStatus.OK);
+    }
     
 }
