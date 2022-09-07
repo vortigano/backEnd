5c7aadc0b40d5367b8cd0c1a3a5e9434b4bcf968
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-09-06 22:21:23 -0300

    cors desde application.properties

--- src/main/java/com/porfolio/cam/Controller/PersonaController.java
+++ src/main/java/com/porfolio/cam/Controller/PersonaController.java
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"${settings.cors_origin}"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
