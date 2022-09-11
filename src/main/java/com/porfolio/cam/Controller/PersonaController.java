cc5f5736abcae49ed06da6e496a424f152c7df1a
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-09-11 19:29:44 -0300

    Error de tipeo, faltaba una llave de cierre

--- src/main/java/com/porfolio/cam/Controller/PersonaController.java
+++ src/main/java/com/porfolio/cam/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"${settings.cors_origin.remote}","${settings.cors_origin.local"})
+@CrossOrigin(origins = {"${settings.cors_origin.remote}","${settings.cors_origin.local}"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
